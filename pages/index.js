3157cdae0795875eb7da145a49d172928d509982
paulcampbell <paulgcampbell@gmail.com>
2020-04-01 22:03:10 +0100

    twitter tags

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ export default function Index() {
     <div>
       <Head>
         <title>NHS Staff Offers</title>
+        <meta name="twitter:title" content="NHS staff offers and discounts" /> 
+        <meta name="twitter:description" content="NHS workers have been inundated with kind offers of support from a wide range of companies – from discounted taxi rides, to dedicated supermarket shopping times, to free food and discounted products." /> 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
