75884575e7d4b571ed7410c6a970dae4673f9ff0
paulcampbell <paulgcampbell@gmail.com>
2020-04-01 21:03:45 +0100

    add some links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Head from 'next/head'
 const offers = require('../offers.js')
 
 export default function Index() {
   return (
     <div>
+      <Head>
+        <title>NHS Staff Offers</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1>List of NHS staff offers</h1>
       <p>NHS workers have been inundated with kind offers of support from a wide range of companies – from discounted taxi rides, to dedicated supermarket shopping times, to free food and discounted products.</p>
-      <p>On the NHS Website, they&apos;re hidden away in an excel document, which isn&apos;t great if you want to check it quickly from your phone or whatever.</p>
+      <p>On the <a href="https://www.england.nhs.uk/coronavirus/publication/list-of-nhs-staff-offers/">NHS Website</a>, they&apos;re <a href="https://www.england.nhs.uk/coronavirus/wp-content/uploads/sites/52/2020/03/NHS-Freebies-FTI-Spreadsheet-27.03.20.xlsx">hidden away in an excel document</a>, which isn&apos;t great if you want to check it quickly from your phone or whatever.</p>
       <p>Hopefully this is a bit easier!</p>
     { Object.keys(offers).map(category => {
       return <div>
